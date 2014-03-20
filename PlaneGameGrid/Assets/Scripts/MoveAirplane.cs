6f863cc8fcb4ed584c8c856bdb015e4491f79a46
Kyle Yount <electrizcity@gmail.com>
2014-03-20 15:50:50 -0700

    NOW I added all the files. Sorry Edward.

---
+++ PlaneGameGrid/Assets/Scripts/MoveAirplane.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAirplane : MonoBehaviour {
+
+	public float interval;
+	private bool keyCheck = false; //checks whether the button was held down last frame
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float moveVertical = Input.GetAxisRaw ("Vertical") * interval;
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal") * interval;
+		
+		if (Input.anyKeyDown == true && keyCheck == false)
+		{
+			keyCheck = true;
+			transform.Translate (moveHorizontal, moveVertical, 0);
+		} 
+		else
+		{
+			keyCheck = false;
+		}
+		
+	}
+}
+
