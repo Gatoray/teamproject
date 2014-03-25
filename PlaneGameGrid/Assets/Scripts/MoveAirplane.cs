49cdab9d1eb494faefc1e66cc90599241964e90f
Edward Kim <edward.m.kim@biola.edu>
2014-03-25 15:00:23 -0700

    I set the boundary to the plane so that it won't go beyond the fixed grid. (Uploaded: 3/25/14)

--- PlaneGameGrid/Assets/Scripts/MoveAirplane.cs
+++ PlaneGameGrid/Assets/Scripts/MoveAirplane.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MoveAirplane : MonoBehaviour {
 
@@ -9,6 +10,14 @@ public class MoveAirplane : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+		// Boundary to ensure that the plane does not go outside its fixed boundary
+		// EDITED BY EDWARD KIM (03/24/14)
+		Vector3 bound = transform.position;
+		bound.x = Mathf.Clamp(bound.x, -36.0f, 36.0f);
+		bound.y = Mathf.Clamp(bound.y, -36.0f, 36.0f);
+		bound.z = Mathf.Clamp(bound.z, 0.0f, 0.0f);
+		transform.position = bound;
+
 		float moveVertical = Input.GetAxisRaw ("Vertical") * interval;
 		float moveHorizontal = Input.GetAxisRaw ("Horizontal") * interval;
 		
@@ -21,7 +30,9 @@ public class MoveAirplane : MonoBehaviour {
 		{
 			keyCheck = false;
 		}
-		
 	}
 }
 
+
+
+// The type or namespace could not be found. Are you missing a using directive or an assembly reference?
